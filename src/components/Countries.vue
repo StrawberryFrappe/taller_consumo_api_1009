db57d84ec6d95c6ceceaba4dfed362b7562422eb
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 14:58:14 -0300

    Create Countries.vue

---
+++ src/components/Countries.vue
@@ -0,0 +1,22 @@
+<script setup>
+import axios from 'axios'
+import {ref} from "vue";
+
+const responses = ref([])
+const show = ref(false)
+function fetch(){
+  axios.get('https://countriesnow.space/api/v0.1/countries/population').then(response =>{
+    responses.value = response.data
+  })
+  show.value = !show.value
+}
+</script>
+
+<template>
+  <button @click="fetch">countries</button>
+  <p v-show="show">{{responses}}</p>
+</template>
+
+<style scoped>
+
+</style>
