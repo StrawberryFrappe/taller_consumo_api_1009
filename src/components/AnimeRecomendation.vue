e6509f48979c039b4d90334af1a53751569c5283
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 15:53:31 -0300

    Update AnimeRecomendation.vue

--- src/components/AnimeRecomendation.vue
+++ src/components/AnimeRecomendation.vue
@@ -9,7 +9,9 @@ const show = ref(false)
 
 function fetch(){
   axios.get('https://api.jikan.moe/v4/recommendations/anime').then(response =>{
-    responses.value = response.data.data
+    for (const el of response.data.data){
+      responses.value.push(el)
+    }
   })
   show.value = !show.value
 
