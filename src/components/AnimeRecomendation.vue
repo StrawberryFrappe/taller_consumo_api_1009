909396ebebe21b3188251cb02bedea8d1fc3f06b
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 14:58:10 -0300

    Create AnimeRecomendation.vue

---
+++ src/components/AnimeRecomendation.vue
@@ -0,0 +1,22 @@
+<script setup>
+import axios from 'axios'
+import {ref} from "vue";
+
+const responses = ref("")
+const show = ref(false)
+function fetch(){
+  axios.get('https://api.jikan.moe/v4/recommendations/anime').then(response =>{
+    responses.value = response.data
+  })
+  show.value = !show.value
+}
+</script>
+
+<template>
+  <button @click="fetch">anime recomend</button>
+  <p v-show="show">{{responses}}</p>
+</template>
+
+<style scoped>
+
+</style>
