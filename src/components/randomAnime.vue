64f1476deb34bad2e367e36112397d42061c68e0
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 14:58:18 -0300

    Create randomAnime.vue

---
+++ src/components/randomAnime.vue
@@ -0,0 +1,22 @@
+<script setup>
+import axios from 'axios'
+import {ref} from "vue";
+
+const responses = ref([])
+const show = ref(false)
+function fetch(){
+  axios.get('https://api.jikan.moe/v4/random/anime').then(response =>{
+    responses.value = response.data
+  })
+  show.value = !show.value
+}
+</script>
+
+<template>
+  <button @click="fetch">random anime</button>
+  <p v-show="show">{{responses}}</p>
+</template>
+
+<style scoped>
+
+</style>
