9f02f04e2320744e23ca4e2e14bd3dde838f40b7
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 15:52:07 -0300

    Update AnimeRecomendation.vue

--- src/components/AnimeRecomendation.vue
+++ src/components/AnimeRecomendation.vue
@@ -2,19 +2,27 @@
 import axios from 'axios'
 import {ref} from "vue";
 
-const responses = ref("")
+const display = ref('')
+const img = ref('')
+const responses = ref([])
 const show = ref(false)
+
 function fetch(){
   axios.get('https://api.jikan.moe/v4/recommendations/anime').then(response =>{
-    responses.value = response.data
+    responses.value = response.data.data
   })
   show.value = !show.value
+
 }
+
+
+
 </script>
 
 <template>
   <button @click="fetch">anime recomend</button>
-  <p v-show="show">{{responses}}</p>
+  <p v-show="show" v-for="item in responses">{{item}}</p>
+  <img :src="display"></img>
 </template>
 
 <style scoped>
