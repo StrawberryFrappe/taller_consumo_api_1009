2f05f23f0fc54913eb634feabd0a218212f4e60c
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-09 14:57:52 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AnimeRecomendation from "@/components/AnimeRecomendation.vue";
+import Countries from "@/components/Countries.vue";
+import randomAnime from '@/components/randomAnime.vue'
+import randomManga from '@/components/randomManga.vue'
+</script>
+
+<template>
+  <main>
+    <AnimeRecomendation></AnimeRecomendation>
+    <Countries></Countries>
+    <randomAnime></randomAnime>
+    <randomManga></randomManga>
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
